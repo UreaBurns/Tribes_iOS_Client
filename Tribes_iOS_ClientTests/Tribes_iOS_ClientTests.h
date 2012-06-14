34a58fd2010fb05acc70458fc081f8803cb058e9
Carmelo Uria <curia@carmelouria.com>
2012-06-13 18:27:51 -0700

    Adding initial files
    Its a shame to can't create the files first

---
+++ Tribes_iOS_ClientTests/Tribes_iOS_ClientTests.h
@@ -0,0 +1,13 @@
+//
+//  Tribes_iOS_ClientTests.h
+//  Tribes_iOS_ClientTests
+//
+//  Created by Carmelo I. Uria on 6/13/12.
+//  Copyright (c) 2012 Carmelo Uria Corporation. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Tribes_iOS_ClientTests : SenTestCase
+
+@end
---
+++ Tribes_iOS_ClientTests/Tribes_iOS_ClientTests.m
@@ -0,0 +1,32 @@
+//
+//  Tribes_iOS_ClientTests.m
+//  Tribes_iOS_ClientTests
+//
+//  Created by Carmelo I. Uria on 6/13/12.
+//  Copyright (c) 2012 Carmelo Uria Corporation. All rights reserved.
+//
+
+#import "Tribes_iOS_ClientTests.h"
+
+@implementation Tribes_iOS_ClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Tribes_iOS_ClientTests");
+}
+
+@end
